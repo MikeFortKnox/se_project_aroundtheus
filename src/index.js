fc3589f961f379f47acfbc8d168467e26ae7cc7b
MikeFortKnox <134332447+MikeFortKnox@users.noreply.github.com>
2024-08-07 16:55:22 -0400

    error fix for image and font processing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import "./styles/index.css";
+import "./index.css";
 import stepsSrc from "./images/steps.png";
 
-console.log("Hello, World!");
 const numbers = [2, 3, 5];
-
-// Arrow function. How will Internet Explorer cope with it?
 const doubledNumbers = numbers.map((number) => number * 2);
 
 console.log(doubledNumbers); // 4, 6, 10
